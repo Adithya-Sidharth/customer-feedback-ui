f9fabd4811cc859166ad28b18729b928765bcb5b
Adithya-Sidharth <adithyasid2002@gmail.com>
2025-11-17 23:51:00 +0530

    Remove next-themes usage from Toaster and main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import App from "./App";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <ThemeProvider attribute="class">
+      <ThemeProvider>
         <App />
       </ThemeProvider>
     </BrowserRouter>
